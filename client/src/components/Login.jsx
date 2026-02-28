b473e1fde6533cf089df890555b05934441c808a
Coder - Sulyman <sulyman560@gmail.com>
2026-02-28 16:37:04 +0600

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
   return (
     <div onClick={()=> setShowUserLogin(false)} className='fixed top-0 bottom-0 left-0 right-0 z-30 flex items-center text-sm text-gray-600 bg-black/50'>
        
-       <form onSubmit={onSubmitHanlar} onClick={(e)=>e.stopPropagation()} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
+       <form onSubmit={onSubmitHanlar} onClick={(e)=>e.stopPropagation()} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-700 bg-white">
 
             <p className="text-2xl font-medium m-auto">
 
