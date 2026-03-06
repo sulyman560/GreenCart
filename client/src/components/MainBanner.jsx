5c6c059a31ad5be51035c7da6ee3c760b009a873
Coder - Sulyman <sulyman560@gmail.com>
2026-03-06 23:36:39 +0600

    Update MainBanner.jsx

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -9,7 +9,7 @@ const MainBanner = () => {
       <img src={assets.main_banner_bg_sm} alt="banner" className='w-full md:hidden' />
       <div className='absolute flex inset-0 flex-col items-center md:items-start justify-end md:justify-center
        pb-24 px-4  lg:pl-24'>
-        <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105
+        <h1 className='text-3xl text-black md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105
          leading-tight lg:leading-15'>Freshness You Can Trust, Savings You will Love! </h1>
 
         <div className='flex items-center mt-6 font-medium'>
